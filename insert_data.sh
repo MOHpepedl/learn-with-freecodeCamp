77f85bf5a6a32fb42d77ca442b10cb5e38b11e4b
Mohamed Alsharfy <149276532+MOHpepedl@users.noreply.github.com>
2026-01-02 02:59:06 +0300

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,48 +1,33 @@
-#! /bin/bash
+#!/bin/bash
 
-if [[ $1 == "test" ]]
-then
-  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
-else
-  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-fi
-echo 
-# Do not change code above this line. Use the PSQL variable above to query your database.
+PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
 
-# إفراغ الجداول الحالية
-echo $($PSQL "TRUNCATE TABLE games, teams")
+# إفراغ الجداول
+echo $($PSQL "TRUNCATE teams, games RESTART IDENTITY")
 
 # قراءة ملف CSV
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  # تخطي السطر الأول (العناوين)
+  # تخطي السطر الأول
   if [[ $YEAR != "year" ]]
   then
-    # إضافة الفريق الفائز إذا لم يكن موجوداً
+    # الحصول على/إضافة الفريق الفائز
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    
     if [[ -z $WINNER_ID ]]
     then
-      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-      if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
-      then
-        WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-      fi
+      INSERT_WINNER=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER') RETURNING team_id")
+      WINNER_ID=$(echo $INSERT_WINNER | sed 's/[^0-9]*//g')
     fi
 
-    # إضافة الفريق الخصم إذا لم يكن موجوداً
+    # الحصول على/إضافة الفريق الخصم
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    
     if [[ -z $OPPONENT_ID ]]
     then
-      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
-      then
-        OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-      fi
+      INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT') RETURNING team_id")
+      OPPONENT_ID=$(echo $INSERT_OPPONENT | sed 's/[^0-9]*//g')
     fi
 
     # إضافة المباراة
-    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    INSERT_GAME=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
 done
